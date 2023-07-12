63983986f0b95452aa10cdcc60094b6ef9b02300
Semih Orhan <sorhan3.14@gmail.com>
2023-07-12 18:03:55 +0300

    install mongose

--- index.js
+++ index.js
@@ -7,15 +7,14 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors());
-
+app.use(express.json());
 app.use('/public', express.static(`${process.cwd()}/public`));
 
 app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-// Your first API endpoint
-app.get('/api/hello', function(req, res) {
+app.post('/api/shorturl', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
 
